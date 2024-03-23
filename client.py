29e5cbe5ad4f8379d65bda9359f1f8beff837608
makam <shammahchakwizira304@gmail.com>
2024-03-23 22:27:41 +0200

    init commit

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket
+
+client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+client.connect(("0C:54:15:E1:86:7D", 4))
+
+try:
+    
+    while True:
+      message= input("Enter message: ")
+      client.send(message.encode("utf-8"))
+      data = client.recv(1024)
+      if not data:
+        break
+      print(f"Message: {data.decode('utf-8')}")
+
+except OSError as e:
+  pass
+
+client.close()
+server.close()
